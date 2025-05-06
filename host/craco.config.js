e53b619ab71f096f965ef43e4e055db5dabf1fca
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-05-06 17:39:08 +0330

    craco

--- host/craco.config.js
+++ host/craco.config.js
@@ -1,7 +1,30 @@
+const deps = require("./package.json").dependencies;
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Modify webpackConfig here
+      const { ModuleFederationPlugin } = require("webpack").container;
+
+      webpackConfig.plugins.push(
+        new ModuleFederationPlugin({
+          name: "host",
+          remotes: {
+            remote: "remote@http://localhost:3001/remoteEntry.js",
+          },
+          shared: {
+            ...deps,
+            react: {
+              singleton: true,
+              requiredVersion: deps.react,
+            },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: deps["react-dom"],
+            },
+          },
+        })
+      );
+
       return webpackConfig;
     },
   },
